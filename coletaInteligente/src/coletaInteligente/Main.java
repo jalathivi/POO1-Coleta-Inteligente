3a7df9d3a2dc3557c897155a5eabdd5d06e13fc2
Lavinia Corteletti <32305721+lvnc@users.noreply.github.com>
2019-04-05 10:59:50 -0300

    Add files via upload

---
+++ coletaInteligente/src/coletaInteligente/Main.java
@@ -0,0 +1,25 @@
+﻿package coletaInteligente;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String codigo = JOptionPane.showInputDialog("Digite o Código do Cliente ");
+        String nome = JOptionPane.showInputDialog("Digite o Nome do Cliente");
+        String email = JOptionPane.showInputDialog("Digite o E-mail");
+        String senha = JOptionPane.showInputDialog("Digite a Senha");
+        String latitude = JOptionPane.showInputDialog("Digite a Latitude");
+        String longitude = JOptionPane.showInputDialog("Digite a Longitude");
+        
+        Cidadao cidadao = new Cidadao (codigo, nome, email, senha, latitude, longitude);
+        
+        /*
+        cidadao.printa(cidadao);
+        */
+        
+        PersistenciaArquivo salvar = new PersistenciaArquivo();
+        salvar.salvarCidadao(cidadao);
+    }
+    
+}
