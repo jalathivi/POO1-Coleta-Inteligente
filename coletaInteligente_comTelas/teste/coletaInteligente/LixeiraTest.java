cffe7718adeb9fca664959f713fe5506662179ce
Jackson Willian <jacksonwilliansilvaagostinho@gmail.com>
2019-05-22 23:30:44 -0300

    atualização

--- coletaInteligente_comTelas/teste/coletaInteligente/LixeiraTest.java
+++ coletaInteligente_comTelas/teste/coletaInteligente/LixeiraTest.java
@@ -41,11 +41,15 @@ public class LixeiraTest {
     
     @Test
     public void testSetCodigo(){
+        
+        System.out.println("setCodigoValido");
         setCodigoValido("369258");
         setCodigoValido("666");
         setCodigoValido("666");
         setCodigoValido("42");
         setCodigoValido("0");
+        
+        System.out.println("setCodigoInvalido");
         setCodigoInvalido("FuitCarregadoNesseTrab");
         setCodigoInvalido(" ");
         setCodigoInvalido("!@#");
@@ -53,7 +57,7 @@ public class LixeiraTest {
     }
     
     public void setCodigoValido(String codigo){
-        System.out.println("setCodigoValido");
+       
         Lixeira instance = new Lixeira();
    
         try {
@@ -61,16 +65,16 @@ public class LixeiraTest {
             
         } catch (Exception ex) {
             Logger.getLogger(LixeiraTest.class.getName()).log(Level.SEVERE, null, ex);
-            fail("Teste Falhou: Inputs válidos não foram aceitos!\n");
+            fail("Teste Falhou: Input válido não foi aceito!\n");
         }
     }
     public void setCodigoInvalido(String codigo){
-        System.out.println("setCodigoValido");
+        
         Lixeira instance = new Lixeira();
 
         try {         
             instance.setCodigo(codigo);
-            fail("Teste Falhou: Inputs inválidos foram aceitos!\n");
+            fail("Teste Falhou: Input inválido foi aceito!\n");
 
         } catch (Exception ex) {
             Logger.getLogger(LixeiraTest.class.getName()).log(Level.SEVERE, null, ex);
@@ -79,10 +83,14 @@ public class LixeiraTest {
     
    @Test
     public void testSetLatitude() {
+        
+        System.out.println("setLatitudeValido");
         setLatitudeValido("90.000");
         setLatitudeValido("-65.8485");
         setLatitudeValido("-40.42200");
         setLatitudeValido("00");
+        
+        System.out.println("setLatitudeInvalido");
         setLatitudeInvalido("180");
         setLatitudeInvalido("325oi1265");
         setLatitudeInvalido("jujuba");
@@ -90,7 +98,7 @@ public class LixeiraTest {
     }
         
     public void setLatitudeValido(String latitude){
-        System.out.println("setLatitudeValido");
+     
         Lixeira instance = new Lixeira();
    
         try {         
@@ -105,7 +113,7 @@ public class LixeiraTest {
     
     
     public void setLatitudeInvalido(String latitude){
-        System.out.println("setLatitudeInvalido");
+       
         Lixeira instance = new Lixeira();
    
         try {         
@@ -119,10 +127,14 @@ public class LixeiraTest {
     }
     @Test
     public void testSetLongitude() {
+        
+        System.out.println("setLongitudeValido");
         setLongitudeValido("90.000");
         setLongitudeValido("-65.8485");
         setLongitudeValido("-40.42200");
         setLongitudeValido("-180");
+    
+        System.out.println("setLongitudeInvalido");
         setLongitudeInvalido("360.0065");
         setLongitudeInvalido("325oi1265");
         setLongitudeInvalido("jujuba");
@@ -130,7 +142,7 @@ public class LixeiraTest {
     }
         
     public void setLongitudeValido(String longitude){
-        System.out.println("setLongitudeValido");
+        
         Lixeira instance = new Lixeira();
    
         try {         
@@ -144,7 +156,7 @@ public class LixeiraTest {
     }
      
     public void setLongitudeInvalido(String longitude){
-        System.out.println("setLongitudeInvalido");
+       
         Lixeira instance = new Lixeira();
    
         try {         
@@ -158,16 +170,20 @@ public class LixeiraTest {
     }  
     @Test
     public void testSetCapacidade() {
+        
+        System.out.println("setLongitudeValido");
         setCapacidadeValido("100");
         setCapacidadeValido("60");
         setCapacidadeValido("40.5");
         setCapacidadeValido("1");
+        
+        System.out.println("setLongitudeInvalido");
         setCapacidadeInvalido("cheio");
         setCapacidadeInvalido("-40");
         setCapacidadeInvalido("0");
     }
     public void setCapacidadeValido(String capacidade){
-        System.out.println("setLongitudeValido");
+        
         Lixeira instance = new Lixeira();
    
         try {         
@@ -181,7 +197,7 @@ public class LixeiraTest {
     }
  
     public void setCapacidadeInvalido(String capacidade){
-        System.out.println("setLongitudeInvalido");
+        
         Lixeira instance = new Lixeira();
    
         try {         
@@ -196,17 +212,21 @@ public class LixeiraTest {
     
     @Test
     public void testSetNivelAtual() {
+        
+        System.out.println("setLongitudeValido");
         setNivelAtualValido("100");
         setNivelAtualValido("60");
         setNivelAtualValido("40.5");
         setNivelAtualValido("0");
+        
+        System.out.println("setLongitudeInvalido");
         setNivelAtualInvalido("cheia");
         setNivelAtualInvalido("maromeno");
         setNivelAtualInvalido("-40");
     }
     
     public void setNivelAtualValido(String nivelAtual){
-        System.out.println("setLongitudeValido");
+        
         Lixeira instance = new Lixeira();
    
         try {         
@@ -221,7 +241,7 @@ public class LixeiraTest {
     
     
     public void setNivelAtualInvalido(String nivelAtual){
-        System.out.println("setLongitudeInvalido");
+        
         Lixeira instance = new Lixeira();
    
         try {         
