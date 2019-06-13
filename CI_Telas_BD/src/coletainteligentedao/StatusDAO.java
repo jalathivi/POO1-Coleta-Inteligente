96887c4ee10fc0405962aacc4bf6af9e84ce8364
Lavinia Corteletti <laviniacort@gmail.com>
2019-06-13 15:11:21 -0300

    poo->bd status

---
+++ CI_Telas_BD/src/coletainteligentedao/StatusDAO.java
@@ -0,0 +1,180 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coletainteligentedao;
+
+import conexao.ConexaoDB;
+import coletainteligente.Status;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Lavinia
+ */
+public class StatusDAO {
+    public void insere(Status b){
+        
+        Connection con = ConexaoDB.getConexao();
+        PreparedStatement stmt = null;
+        
+        try {
+            
+            stmt = con.prepareStatement("INSERT INTO status (descricao) VALUES (?);");
+           //stmt.setInt(1, c.getCodigo());
+            stmt.setString(1, b.getDescricao());
+            
+            stmt.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "Insert criado com sucesso");
+            ConexaoDB.closeConnection(con, stmt);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(StatusDAO.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Insert não deu certo!\n" + ex.getMessage()); 
+            
+        } finally {
+            ConexaoDB.closeConnection(con, stmt);
+            
+        }
+    }  
+    
+    /**
+     *Retorna Falso caso não encontre o nome
+     * @param descricao
+     * @return check
+     */
+    
+    public boolean readDescricao(String descricao) {
+        Connection conexao = ConexaoDB.getConexao();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        boolean check = false;
+        
+        try {
+            stmt = conexao.prepareStatement("SELECT * FROM status WHERE descricao = ?;");
+            stmt.setString(1, descricao);
+            rs = stmt.executeQuery();
+            
+           if(rs.next()) {
+               check = true;
+           }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(StatusDAO.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, ex);
+        } finally {
+            ConexaoDB.closeConnection(conexao, stmt);
+        }
+        return check;
+    }
+    
+    
+    public ArrayList selectListaStatus() {
+        Connection conexao = ConexaoDB.getConexao();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        ArrayList dados = new ArrayList();
+
+        try {
+            stmt = conexao.prepareStatement("SELECT * FROM status;");
+            //stmt.setString(1, descricao);
+            rs = stmt.executeQuery();
+            
+           while(rs.next()) {
+               dados.add(rs.getString("descricao"));
+           }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(StatusDAO.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, ex);
+        } finally {
+            ConexaoDB.closeConnection(conexao, stmt);
+        }
+        return dados;
+    }
+ 
+    public ArrayList selectListaCodStatus() {
+        Connection conexao = ConexaoDB.getConexao();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        ArrayList dados = new ArrayList();
+
+        try {
+            stmt = conexao.prepareStatement("SELECT * FROM status;");
+            //stmt.setString(1, descricao);
+            rs = stmt.executeQuery();
+            
+           while(rs.next()) {
+               dados.add(rs.getString("cod_status"));
+           }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(StatusDAO.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, ex);
+        } finally {
+            ConexaoDB.closeConnection(conexao, stmt);
+        }
+        return dados;
+    }
+
+
+    public void deleta(String descricao){
+        
+        Connection con = ConexaoDB.getConexao();
+        PreparedStatement stmt = null;
+        
+        try {
+            System.out.print(descricao);
+            stmt = con.prepareStatement("DELETE FROM status WHERE descricao=?");
+            stmt.setString(1, descricao);
+            
+            stmt.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "Exclusão realizado com sucesso");
+            ConexaoDB.closeConnection(con, stmt);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(StatusDAO.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Exclusão não deu certo!\n" + ex.getMessage()); 
+            
+        } finally {
+            ConexaoDB.closeConnection(con, stmt);
+            
+        }
+    }    
+    
+    public void alteraBairro(String descricaoOld, String descricao){
+        
+        Connection con = ConexaoDB.getConexao();
+        PreparedStatement stmt = null;
+        
+        try {
+            
+            stmt = con.prepareStatement("UPDATE status SET descricao=? WHERE descricao=?");
+            stmt.setString(1, descricao);
+            stmt.setString(2, descricaoOld);
+            
+            stmt.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "Update realizado com sucesso");
+            ConexaoDB.closeConnection(con, stmt);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(StatusDAO.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Update não deu certo!\n" + ex.getMessage()); 
+            
+        } finally {
+            ConexaoDB.closeConnection(con, stmt);
+            
+        }
+    }
+}
--- CI_Telas_BD/src/visao/TelaCadastroStatus.java
+++ CI_Telas_BD/src/visao/TelaCadastroStatus.java
@@ -7,6 +7,8 @@ package visao;
 
 import coletainteligente.Status;
 import coletainteligente.PersistenciaArquivo;
+import coletainteligentedao.StatusDAO;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;     
@@ -35,11 +37,19 @@ public class TelaCadastroStatus extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jTextFieldDescricao = new javax.swing.JTextField();
-        jButtonRegistrarStatus = new javax.swing.JButton();
+        campoDescricaoStatus = new javax.swing.JTextField();
+        jButtonSalvar = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         jButtonLimpar = new javax.swing.JButton();
         jButtonSair = new javax.swing.JButton();
+        jButtonDeletar = new javax.swing.JButton();
+        jButtonAlterar = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jListStatus = new javax.swing.JList();
+        jLabel3 = new javax.swing.JLabel();
+        jButtonViewStatus = new javax.swing.JButton();
+        campoNovaDescricao = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Registrar Status");
@@ -47,14 +57,14 @@ public class TelaCadastroStatus extends javax.swing.JFrame {
 
         jLabel1.setText("Cadastro Status");
 
-        jButtonRegistrarStatus.setText("Registrar Status");
-        jButtonRegistrarStatus.addActionListener(new java.awt.event.ActionListener() {
+        jButtonSalvar.setText("Cadastrar");
+        jButtonSalvar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButtonRegistrarStatusActionPerformed(evt);
+                jButtonSalvarActionPerformed(evt);
             }
         });
 
-        jLabel2.setText("Descrição");
+        jLabel2.setText("Novo Status");
 
         jButtonLimpar.setText("Limpar");
         jButtonLimpar.addActionListener(new java.awt.event.ActionListener() {
@@ -70,83 +80,162 @@ public class TelaCadastroStatus extends javax.swing.JFrame {
             }
         });
 
+        jButtonDeletar.setText("Deletar");
+        jButtonDeletar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonDeletarActionPerformed(evt);
+            }
+        });
+
+        jButtonAlterar.setText("Alterar");
+        jButtonAlterar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonAlterarActionPerformed(evt);
+            }
+        });
+
+        jListStatus.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
+            public void valueChanged(javax.swing.event.ListSelectionEvent evt) {
+                jListStatusValueChanged(evt);
+            }
+        });
+        jScrollPane1.setViewportView(jListStatus);
+
+        jLabel3.setText("Status");
+
+        jButtonViewStatus.setText("Ver Status");
+        jButtonViewStatus.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonViewStatusActionPerformed(evt);
+            }
+        });
+
+        campoNovaDescricao.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                campoNovaDescricaoActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setText("Novo Status");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(jLabel1)
-                .addGap(156, 156, 156))
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(40, 40, 40)
+                .addGap(103, 103, 103)
+                .addComponent(jLabel1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jButtonRegistrarStatus)
-                        .addGap(34, 34, 34)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(jLabel2)
+                                .addGap(18, 18, 18)
+                                .addComponent(campoDescricaoStatus, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addComponent(jLabel3)
+                                        .addGap(34, 34, 34))
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addComponent(jButtonViewStatus)
+                                        .addGap(2, 2, 2)))
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel4)
+                            .addComponent(jButtonDeletar)
+                            .addComponent(jButtonSalvar)
+                            .addComponent(campoNovaDescricao, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(jButtonSair)
+                                .addComponent(jButtonAlterar))))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addGap(0, 218, Short.MAX_VALUE)
                         .addComponent(jButtonLimpar)
-                        .addGap(18, 18, 18)
-                        .addComponent(jButtonSair))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel2)
-                        .addGap(18, 18, 18)
-                        .addComponent(jTextFieldDescricao, javax.swing.GroupLayout.PREFERRED_SIZE, 246, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(49, Short.MAX_VALUE))
+                        .addGap(139, 139, 139)))
+                .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
-                .addGap(76, 76, 76)
+                .addGap(32, 32, 32)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(34, 34, 34)
+                        .addComponent(jButtonDeletar)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(campoNovaDescricao, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jButtonAlterar))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(campoDescricaoStatus, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jButtonSalvar))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(18, 18, 18)
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(81, 81, 81)
+                                .addComponent(jLabel3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButtonViewStatus)))))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextFieldDescricao, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 102, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButtonRegistrarStatus)
                     .addComponent(jButtonLimpar)
                     .addComponent(jButtonSair))
-                .addGap(26, 26, 26))
+                .addGap(29, 29, 29))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
-        setSize(new java.awt.Dimension(416, 346));
+        setSize(new java.awt.Dimension(435, 346));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButtonRegistrarStatusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonRegistrarStatusActionPerformed
-        // TODO add your handling code here:
+    private void jButtonSalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonSalvarActionPerformed
+        StatusDAO statusDAO = new StatusDAO();
         
         PersistenciaArquivo registro = new PersistenciaArquivo();
-        String descricao = jTextFieldDescricao.getText();
+        
+        String descricao = campoDescricaoStatus.getText();
 
         try {
             
             Status status = new Status(descricao);
+            statusDAO.insere(status);
             registro.salvaStatus(status);
             JOptionPane.showMessageDialog(null, "Status cadastrado com sucesso!");
-            this.dispose();
+            jListStatus.setListData(statusDAO.selectListaStatus().toArray());
+            campoDescricaoStatus.setText("");
 
         } catch (Exception ex){
             Logger.getLogger(TelaCadastroStatus.class.getName()).log(Level.SEVERE, null, ex);
             JOptionPane.showMessageDialog(null, ex.getMessage());
         }
-    }//GEN-LAST:event_jButtonRegistrarStatusActionPerformed
+    }//GEN-LAST:event_jButtonSalvarActionPerformed
 
     private void jButtonSairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonSairActionPerformed
         // TODO add your handling code here:
-        if (! jTextFieldDescricao.getText().equals("")){
+        if (! campoDescricaoStatus.getText().equals("")){
             int resposta = JOptionPane.showConfirmDialog(null, "Há campos preenchidos!\nTem certeza que deseja sair dessa tela?");
         
            if (resposta == JOptionPane.YES_OPTION){
@@ -159,9 +248,51 @@ public class TelaCadastroStatus extends javax.swing.JFrame {
 
     private void jButtonLimparActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonLimparActionPerformed
         // TODO add your handling code here:
-        jTextFieldDescricao.setText("");
+        campoDescricaoStatus.setText("");
+        campoNovaDescricao.setText("");
     }//GEN-LAST:event_jButtonLimparActionPerformed
 
+    private void jButtonViewStatusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonViewStatusActionPerformed
+        // TODO add your handling code here:
+        StatusDAO statusDAO = new StatusDAO();
+        jListStatus.setListData(statusDAO.selectListaStatus().toArray());
+        
+    }//GEN-LAST:event_jButtonViewStatusActionPerformed
+
+    private void campoNovaDescricaoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_campoNovaDescricaoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_campoNovaDescricaoActionPerformed
+
+    private void jListStatusValueChanged(javax.swing.event.ListSelectionEvent evt) {//GEN-FIRST:event_jListStatusValueChanged
+        // TODO add your handling code here:
+        JOptionPane.showMessageDialog(null, "Selecionado:  " + jListStatus.getSelectedValue().toString());
+        campoNovaDescricao.setText(null);
+    }//GEN-LAST:event_jListStatusValueChanged
+
+    private void jButtonDeletarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonDeletarActionPerformed
+        // TODO add your handling code here:
+        StatusDAO statusDAO = new StatusDAO();
+        int index = jListStatus.getSelectedIndex();
+        ArrayList dados = statusDAO.selectListaStatus();
+        statusDAO.deleta((String) dados.get(index));
+        jListStatus.setListData(statusDAO.selectListaStatus().toArray());
+    }//GEN-LAST:event_jButtonDeletarActionPerformed
+
+    private void jButtonAlterarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonAlterarActionPerformed
+        // TODO add your handling code here:
+        StatusDAO statusDAO = new StatusDAO();
+        int index = jListStatus.getSelectedIndex();
+        ArrayList dados = statusDAO.selectListaStatus();
+        if(("".equals(campoNovaDescricao.getText())) || index == -1 ) {
+            JOptionPane.showMessageDialog(null, "Insira uma descrição");
+        }else{
+            statusDAO.alteraBairro((String) dados.get(index), campoNovaDescricao.getText());
+            campoNovaDescricao.setText(null);
+            jListStatus.setListData(statusDAO.selectListaStatus().toArray());
+        }
+    }//GEN-LAST:event_jButtonAlterarActionPerformed
+
+    
     /**
      * @param args the command line arguments
      */
@@ -198,12 +329,20 @@ public class TelaCadastroStatus extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField campoDescricaoStatus;
+    private javax.swing.JTextField campoNovaDescricao;
+    private javax.swing.JButton jButtonAlterar;
+    private javax.swing.JButton jButtonDeletar;
     private javax.swing.JButton jButtonLimpar;
-    private javax.swing.JButton jButtonRegistrarStatus;
     private javax.swing.JButton jButtonSair;
+    private javax.swing.JButton jButtonSalvar;
+    private javax.swing.JButton jButtonViewStatus;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JList jListStatus;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JTextField jTextFieldDescricao;
+    private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 }
